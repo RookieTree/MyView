53f80e6843851eea22304c66871a81b1464eb039
tree <773555725@qq.com>
2023-03-14 17:10:48 +0800

    模拟google 跑马灯特效

---
+++ app/src/main/java/com/tree/myview/activity/LoopActivity.kt
@@ -0,0 +1,51 @@
+package com.tree.myview.activity
+
+import android.util.Log
+import android.view.View
+import android.widget.ImageView
+import androidx.constraintlayout.helper.widget.Carousel
+import coil.load
+import coil.size.Scale
+import com.tree.myview.R
+
+/*
+ *  @项目名：  RulerDemo
+ *  @包名：    com.tree.myview.activity
+ *  @文件名:   LoopActivity
+ *  @创建者:   rookietree
+ *  @创建时间:  2023/3/10 15:46
+ *  @描述：    TODO
+ */
+class LoopActivity : BaseActivity() {
+    private val imgs = intArrayOf(
+        R.drawable.look1,
+        R.drawable.look2,
+        R.drawable.look3,
+        R.drawable.look4,
+        R.drawable.look5
+    )
+
+    override fun getLayoutId(): Int {
+        return R.layout.activity_loop
+    }
+
+    override fun init() {
+        val carousel = findViewById<Carousel>(R.id.carousel)
+        carousel.setAdapter(object : Carousel.Adapter {
+            override fun count(): Int {
+                return imgs.size
+            }
+
+            override fun populate(view: View, index: Int) {
+                if (view is ImageView) {
+                    view.setImageResource(imgs[index])
+                }
+                //                Log.d("loopww","populate index:"+index);
+            }
+
+            override fun onNewItem(index: Int) {
+                Log.d("loopww", "onNewItem index:$index")
+            }
+        })
+    }
+}
